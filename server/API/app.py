2930be8276163a2ed640c976514ca472923eb4cf
artfer <artfergames@gmail.com>
2025-05-05 16:35:44 +0700

    fix changes

--- server/API/app.py
+++ server/API/app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import subprocess
 
 app = FastAPI()
 
-@app.get('/')
+def getThermal():
+    with open('/sys/class/thermal/thermal_zone0/temp') as f:
+        return [int(s) for s in f][0] / 1000
+
+def getGitHub():
+    return
+
+@app.get('/api/main')
 async def home():
-    return {'data': 'home'}
+    return {'temp': getThermal()}
